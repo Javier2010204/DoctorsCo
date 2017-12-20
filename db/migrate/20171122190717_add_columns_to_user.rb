fec9d4030356876fb912e70818b98bb3e1e8e33b
JavierSC <2010204javiergomez@gmail.com>
2017-12-20 12:11:15 -0600

    migrations

--- db/migrate/20171122190717_add_columns_to_user.rb
+++ db/migrate/20171122190717_add_columns_to_user.rb
@@ -1,13 +1,9 @@
 class AddColumnsToUser < ActiveRecord::Migration[5.1]
 	def change
-		add_column :users, :avatar_content_type, :string
-		add_column :users, :avatar_file_size, :integer
-		add_column :users, :avatar_updated_at, :datetime
+		
 	end
 
 	def down
-		remove_column :users, :avatar_content_type, :string
-		remove_column :users, :avatar_file_size, :integer
-		remove_column :users, :avatar_updated_at, :datetime
+		
 	end
 end
