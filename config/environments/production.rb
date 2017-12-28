a623218753ceae4f0547ba37d66407f38ad3881e
JavierSC <2010204javiergomez@gmail.com>
2017-12-28 10:49:20 -0600

    Mailer con Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,17 +69,15 @@ Rails.application.configure do
 	# Set this to true and configure the email server for immediate delivery to raise delivery errors.
 	# config.action_mailer.raise_delivery_errors = false
 
-	config.action_mailer.delivery_method = :smtp
-	config.action_mailer.default_url_options = { :host => "https://shielded-chamber-87311.herokuapp.com/" }
 	config.action_mailer.smtp_settings = {
-	      :address => "smtp.gmail.com",
-	      :port => 587,
-	      :domain => 'https://shielded-chamber-87311.herokuapp.com/:80',
-	      :user_name => "doctorsco.net@gmail.com",
-	      :password => "Hola1234!",
-	      :authentication => :plain,
-	      :enable_starttls_auto => true
-	}
+	    :address              => "smtp.gmail.com",
+	    :port                 => 587,
+	    :domain               => "gmail.com",
+	    :user_name            => "doctorsco.net@gmail.com" #your gmail id
+	    :password             => "Hola1234!" #your gmail password
+	    :authentication       => :plain,
+	    :enable_starttls_auto => true
+	  }
 
 	# Enable locale fallbacks for I18n (makes lookups for any locale fall back to
 	# the I18n.default_locale when a translation cannot be found).
